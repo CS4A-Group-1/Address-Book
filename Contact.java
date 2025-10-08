1a963be9927e392af6e05c89c68c3a7771b0d288
Mitch Merrell <themitchguy@gmail.com>
2025-10-07 19:36:38 -0700

    Initial Commit - Contact class created
    Initial Commit
    - Created the Contact class

---
+++ Contact.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+
+public class Contact {
+
+    //=============================
+    // Data fields
+
+    private String name;
+    private String phoneNumber;
+    private String email;
+    private String type;
+    private String city;
+    private ArrayList<String> tags;
+
+    //=============================
+    // Constructors
+
+    public Contact(String name, String email, String phoneNumber, String type, String city) {
+        this(name, phoneNumber);
+        this.email = email;
+        this.type = type;
+        this.city = city;
+    }
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.email = "";
+        this.type = "";
+        this.city = "";
+        this.tags = new ArrayList<>();
+    }
+
+    //=============================
+    // Name
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    //=============================
+    // Phone Number
+
+    public String getPhoneNumber() {
+        return this.phoneNumber;
+    }
+    public void setPhoneNumber(String number) {
+        this.phoneNumber = number;
+    }
+    
+    //=============================
+    // Email
+
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    //=============================
+    // Type
+
+    public String getType() {
+        return this.type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+    //=============================
+    // City
+    
+    public String getCity() {
+        return this.city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    //=============================
+    // Tags
+    
+    public String getTag(int index) {
+        return this.tags.get(index);
+    }
+    public int getIndexOfTag(String tag) {
+        return this.tags.indexOf(tag);
+    }
+    public void addTag(String tag) {
+        this.tags.add(tag);
+    }
+    public void removeTag(int index) {
+        this.tags.remove(index);
+    }
+    public void removeTag(String tag) {
+        this.tags.remove(tag);
+    }
+    public ArrayList<String> getTags() {
+        return this.tags;
+    }
+}
