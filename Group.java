1fb71dfd5ddaf2d111cc2ca3f95dbc4533262551
Kyle Larson <122947142+larsonkyle@users.noreply.github.com>
2025-10-16 15:25:35 -0700

    Made removeContact() return bool to determine if remove was successful.

--- Group.java
+++ Group.java
@@ -16,14 +16,17 @@ public class Group {
         }
     }
 
-    //Remove a contact from the group
-    public void removeContact(Contact contact) {
-        members.remove(contact);
+    //Remove a contact from the group, return 1 or 0 if remove was successful or not
+    // !Current problem! : 
+    // Java remove() checks equality based off "reference equality", so to remove an object you must pass its exact REFERENCE.
+    // could override equals() method in Object class to fix, but contact class is not finished so this is what we got :p (might not be relevant anyways..)
+    public boolean removeContact(Contact contact) {
+        return members.remove(contact);
     }
 
     //Return list of all contacts in this group
     public ArrayList<Contact> listContacts() {
-        return new ArrayList<>(members); 
+        return new ArrayList<>(members); //RETURNS COPY
     }
 
     //Return the name of the group
